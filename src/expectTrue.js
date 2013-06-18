991358f4b89b530cad0242e9f118afaf4d41fe28
Jim Higson <jim@wikizzle.org>
2013-06-18 18:29:55 +0100

    use naga/apply instead of Function#apply

--- src/expectTrue.js
+++ src/expectTrue.js
@@ -1,12 +1,12 @@
-define( ['naga/error/halt'], 
-function(halt) {
+define( ['naga/error/halt', 'naga/apply'], 
+function(halt, apply) {
 
    var wasFalse = halt("expression wasn't true, it was {foundValue}");
 
    return function expectTrue( f ) {
       return function() {
          
-         var found = f.apply( this, arguments ); 
+         var found = apply(f, arguments); 
          
          if( !found ) {
             wasFalse(found);
