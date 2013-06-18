2598500eeac343c9f0c39fd856b0f210a3381597
Jim Higson <jim@wikizzle.org>
2013-06-18 18:30:56 +0100

    tests for is

---
+++ test/cases/isTest.js
@@ -0,0 +1,62 @@
+
+/*
+   var given = Bell.given,
+       is = Bell.is,
+       curry = Naga.curry,
+       prefix = Naga.prefix;
+
+   var adding = curry(prefix('+')); */
+
+DependentTestCase('is',
+   ['bell/is'], 
+   {
+      'test simple case': 
+         function(is){
+            // should pass
+            is(2)(2);
+         }
+         
+   ,  'test simple failing case': 
+         function(is){
+            // should fail
+            is(2)(5);
+         }         
+   }
+);
+
+
+
+// test that an exception thrown in any of the G,W,T fails the test
+// test that no exceptions thrown passes the test
+
+/*
+   given(2)
+      .when(adding(2))
+      .then(is(4)); // passes!
+
+   given(2)
+      .when(adding(2))
+      .then(isEven); // passes!
+
+   given(2)
+      .when(adding(2))
+      .then(is(5))
+      .should(fail.withError("performed 2 + 2, expected 5 but got 4"));  // passed. Error was expected.
+
+   given(1)
+      .when(adding(2))
+      .then(adding(3))
+      .then(adding(4))
+      .is(equalTo(1+2+3+4));  // passes!
+
+   given(1,2,3,4,5)
+      .then(all(lessThan(6)));
+      // Need to convert boolean value from all (Naga) to thrown exception (Bell)
+      // could wrap all functions passed to g/w/t to react in this way. Eg, Bell.condition does this!
+
+
+   given(1,2,3,4,5)
+      .then(all(lessThan(5)))
+      .should(fail);
+      
+   */      
