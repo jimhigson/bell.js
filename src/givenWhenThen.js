f17367aac413b4f14bb588764188af8a18515f76
Jim Higson <jim@wikizzle.org>
2013-06-18 21:01:42 +0100

    allow the given clause to a g.w.t. to not be a function

--- src/givenWhenThen.js
+++ src/givenWhenThen.js
@@ -1,13 +1,18 @@
 define(
-   ['naga/chain', 'naga/compose'],
-   function(chain, compose) {
+   ['naga/chain', 'naga/compose', 'naga/lift'],
+   function(chain, compose, lift) {
+
+      function functionOrLift(maybeFunc) {
+         return ( maybeFunc instanceof Function )? maybeFunc : lift(maybeFunc);      
+      }
 
       // given-when-then is basically functional chaining and composition.
-
       return chain(
          'given(givenFunc).when(whenFunc).then(thenFunc)',
-         function(givenFunc, whenFunc, thenFunc){
+         function(givenMaybeFunc, whenFunc, thenFunc){
 
+            var givenFunc = functionOrLift(givenMaybeFunc);
+            
             // compose and execute the three functions:
             compose(thenFunc, whenFunc, givenFunc)();
          }
